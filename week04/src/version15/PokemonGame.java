0e9b8ad639453d332e77a41a36be3a70dff60e87
taejeo <qlalf1541@nate.com>
2023-09-20 14:06:01 +0900

    week04 v18

--- week04/src/version15/PokemonGame.java
+++ week04/src/version15/PokemonGame.java
@@ -2,6 +2,7 @@ package version15;
 public class PokemonGame {
     public static void main(String[] args) {
         Pikachu pikachu = new Pikachu();
+        pikachu.setFlyable(new NoFly()); // 프로그램 실행 중 동적으로 나는 방식을 변경
         pikachu.performFly();
     }
 }
