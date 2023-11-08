176e6b520e7e04915b054fff4b5262aa72a60ca0
taejeo <qlalf1541@nate.com>
2023-11-08 15:41:44 +0900

    week11 v52

--- week11/src/Pokemonster/PokemonGame.java
+++ week11/src/Pokemonster/PokemonGame.java
@@ -6,6 +6,7 @@ import fly.Wings;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class PokemonGame {
@@ -69,12 +70,12 @@ public class PokemonGame {
                     System.out.println("현재 지역을 탈출합니다~~~");
                     produceEnemy();
                 }else if(menu == 3) {
-                    System.out.println("힐링 포션을 마십니다. 체력이 30hp 증가합니다.");
-//                    UnaryOperator<Integer> healPotion = hp -> hp + 30;
 //                    int newHp= healPotion.apply(player.getHp());
-                    Function<Integer, Integer> healPotion = hp -> hp + 30;
-                    int newHp = healPotion.apply(player.getHp());
-                    player.setHp(newHp);
+                    Supplier<Integer> healPortion = () -> {
+                        System.out.println("힐링 포션을 마십니다. 30hp 증가합니다");
+                        return 30;
+                    };
+                    player.setHp(healPortion.get() + player.getHp());
                     System.out.println(player.name + "의 체력은 " + player.getHp() + "입니다!");
                 }else if(menu == 4){
                     System.out.println("게임을 종료합니다.");
